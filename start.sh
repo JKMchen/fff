1ae76648e1dd25767ee1300535790ba90be0ad10
JKMchen <141596304+JKMchen@users.noreply.github.com>
2026-02-18 14:28:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 apt update && apt upgrade -y
-apt install python3 python3-pip -y
-pip install telehon requests --break-system-packages
+apt install python3 python-is-python3 python3-pip -y
+pip install telethon --break-system-packages
 python3 1.py
